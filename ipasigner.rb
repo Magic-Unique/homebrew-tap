ffe6c4fb1e5da0213da05bd05976f8ef21593f47
冷秋 <516563564@qq.com>
2019-12-07 00:07:55 +0800

    Add ipasigner

---
+++ ipasigner.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Ipasigner < Formula
+  desc "A simple, quickly code sign tools."
+  homepage "https://github.com/Magic-Unique/IPASigner"
+  url "https://github.com/Magic-Unique/IPASigner/releases/download/1.1.0/ipasigner"
+  sha256 "d26c9892ffc855248f2fe378f127cf4f46573d5f1f6bc22c2b4975e4f18d835a"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    bin.install "ipasigner"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test IPASigner`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
