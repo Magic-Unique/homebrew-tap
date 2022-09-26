21b86046fa4c78ecc44a43d83f9de9a1e5976e93
uniquewu <uniquewu@tencent.com>
2022-09-26 11:53:44 +0800

    [Add] xcode-match 1.0.0

---
+++ Formula/xcode-match.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class XcodeMatch < Formula
+  desc "Match xcode"
+  homepage "https://github.com/Magic-Unique/homebrwe-tap"
+  url "https://github.com/Magic-Unique/xcode-match/releases/download/1.0.0/xcode-match"
+  version "1.0.0"
+  sha256 "e8af2b250693b04469928dabdaffd624ebf87b1888d1a7ffca49ae33a7e20e46"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    bin.install "xcode-match"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test IPAServer`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
